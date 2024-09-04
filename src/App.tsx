a12fa00ed27487c152977ce08656836d605ab7e4
SachsAlex <alexander.sachs@docc.techstarter.de>
2024-09-04 14:21:02 +0200

    add_speechBubble+Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./styles/App.css";
+import "./styles/Globals.css";
 import HomePage from "./pages/homepage";
 
 function App() {
--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -2,13 +2,13 @@ import styles from "./HomePage.module.css";
 import guy from "../../mediathek/images/[removal.ai]_49894e9d-3c96-47f6-8cab-c61a0955949b-_07e49ec1-e963-4d2e-9c35-771ebe298fd8b2.png";
 import { useState } from "react";
 import Button from "../../components/common/buttons";
+import "@fontsource/tenor-sans";
 
 function HomePage() {
   const [speechBubble, setSpeechBubble] = useState(false);
 
   function onClick() {
     setSpeechBubble(!speechBubble);
-    console.log("Hallo" + speechBubble);
   }
 
   return (
@@ -21,7 +21,18 @@ function HomePage() {
       />
       {speechBubble && (
         <div className={styles.speechBubble}>
-          <h1>Hallo Welt</h1>
+          <div className={styles.welcome}>
+            Willkommen beim Casino MCXL.
+            <div>
+              Bitte zeigen Sie Ihren Ausweis (
+              <Button onClick={onClick} text="SignUp" id="1" className="" />)
+              <div>
+                oder Ihre Mitgliedskarte (
+                <Button onClick={onClick} text="LogIn" id="2" className="" />
+                ).
+              </div>
+            </div>
+          </div>
         </div>
         // <Button onClick={onClick} text="Hallo Welt" id="1" className="" />
       )}
