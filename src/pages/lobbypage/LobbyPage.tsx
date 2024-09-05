415335225cbfba9c16a44a2839ca2fb229c85ad4
SachsAlex <alexander.sachs@docc.techstarter.de>
2024-09-05 14:30:03 +0200

    update_LobbyPage

--- src/pages/lobbypage/LobbyPage.tsx
+++ src/pages/lobbypage/LobbyPage.tsx
@@ -1,5 +1,5 @@
 import styles from "./LobbyPage.module.css";
-import guy2 from "../../mediathek/images/Carsten05b2.webp";
+import guy2 from "../../mediathek/images/Alex07b1.webp";
 import "@fontsource/tenor-sans";
 
 function LobbyPage() {
@@ -7,9 +7,21 @@ function LobbyPage() {
     <div className={styles.background}>
       <div className={styles.mainContainer}>
         <img className={styles.guy2} src={guy2} alt="Typ" />
-        <div className={styles.speechBubble}>
-          <div className={styles.welcome}>
-            <div className={styles.space}> Willkommen beim Casino MCXL. </div>
+        <div>
+          <div className={styles.speechBubble}>
+            <div className={styles.welcome}>
+              <div className={styles.space}> Willkommen im Casino MCXL. </div>
+              <div className={styles.space}>
+                Was ist Ihr Spiel am heutigen Abend?
+              </div>
+              <div className={styles.space}>
+                Wir haben Roulette genau vor Ihnen,
+              </div>
+              <div className={styles.space}>BlackJack zu Ihrer Linken</div>
+              <div className={styles.space}>
+                und Slot Maschinen mit Aussicht.
+              </div>
+            </div>
           </div>
         </div>
       </div>
