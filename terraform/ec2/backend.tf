fc4f10d3f084d310b2178521cf9139ab383ac19d
SachsAlex <alexander.sachs@docc.techstarter.de>
2024-10-09 11:52:36 +0200

    update_backend ec2

--- terraform/ec2/backend.tf
+++ terraform/ec2/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "casino-2310-bucket"
-    key    = "ec2-casino/ec2.tfstate"
+    key    = "ec2-casino/ec2-frontend.tfstate"
     region = "eu-central-1"
   }
 }
