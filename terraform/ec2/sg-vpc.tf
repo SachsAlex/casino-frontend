56bb60057ed6baad4eb93ee569ff94ef8e9d8272
SachsAlex <alexander.sachs@docc.techstarter.de>
2024-10-10 15:55:43 +0200

    update_for playbook

--- terraform/ec2/sg-vpc.tf
+++ terraform/ec2/sg-vpc.tf
@@ -15,6 +15,12 @@ resource "aws_security_group" "http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 0
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
