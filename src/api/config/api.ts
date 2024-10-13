5effbb1f80642d0a02e2d9502bb3de82bea7e917
SachsAlex <alexander.sachs@docc.techstarter.de>
2024-10-13 14:21:46 +0200

    update_api

--- src/api/config/api.ts
+++ src/api/config/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://18.156.175.1:5050/v1", //IP von Backend EC2
+  baseURL: "http://18.156.175.:5050/v1", //IP von Backend EC2
 });
 
 export default api;
